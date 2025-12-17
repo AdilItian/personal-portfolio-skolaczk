0ba2bde8a230bbe2f201b93de4953651bb32deca
adilfarooq-i-ways <adil.farooq@i-ways.net>
2025-12-17 16:27:51 +0500

    fix build issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export const metadata: Metadata = {
     siteName: siteConfig.title,
     images: [
       {
-        url: '/images/og-image.png', 
+        url: '/images/og-image.png',
         width: 1200,
         height: 630,
         alt: siteConfig.title,
